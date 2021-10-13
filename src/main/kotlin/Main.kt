4e1219f998f58f8ab82d5e0c8168446edef86783
Zamel <mohamed.zamel@tuwaiq.edu.sa>
2021-10-13 17:01:02 +0300

    Add Functions in course

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,36 +1,15 @@
+import kotlin.math.ln1p
+
+
 fun main() {
 //sort 3 unique numbers using if
-    var n1 = 4.0
-    var n2 = 1.0
-    var n3 = 0.5
-    var smallest = n1
-    if (smallest >= n2) {
-        smallest = n2
-    }
-    if (smallest >= n3) {
-        smallest = n3
-    }
+    val n1 = 4.0
+    val n2 = 1.0
+    val n3 = 0.5
 
-    var biggest = n1
-    if (biggest <= n2) {
-        biggest = n2
-    }
-    if (biggest <= n3) {
-        biggest = n3
-    }
-
-
-    var mid = 0.0
-    if ((n1 < n2 && n1 > n3) || (n1 > n2 && n1 < n3)) {
-        mid = n1
-    }
-    if ((n2 < n1 && n2 > n3) || (n2 > n1 && n2 < n3)) {
-        mid = n2
-    }
-    if ((n3 < n1 && n3 > n2) || (n3 > n1 && n3 < n2)) {
-        mid = n3
-    }
-
-
+    val smallest = getSmallest(n1, n2, n3)
+    val mid = getEasyMid(n1, n2, n3)
+    val biggest = getBiggest(n1, n2, n3)
     println("$smallest $mid $biggest")
+
 }
