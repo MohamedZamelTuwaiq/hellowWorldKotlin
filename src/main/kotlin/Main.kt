167f8b3335d021209e3575cf3571fd9252c44bee
Zamel <mohamed.zamel@tuwaiq.edu.sa>
2021-10-14 13:26:37 +0300

    single expression

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main(){
+    welcomeStudent("Zamel")
+//val result=isPassed(40)
+//    println(result)
+
+}
+
+//non return single expression
+fun welcomeStudent(studentName:String)= println("Welcome $studentName to our course")
+// return value
+fun getPI():Double{
+    return 3.14
+}
+// return value with single exp
+fun getPISingleWay():Double=3.14
+
+fun isPassed(studentGrade:Int)= studentGrade>=50
+
+
+
