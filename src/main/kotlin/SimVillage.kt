107cfce82afc5b3f1ae562c808b480eb47e7a20b
Zamel <mohamed.zamel@tuwaiq.edu.sa>
2021-10-17 15:55:30 +0300

    --anomyns function function as prams or/ return

--- src/main/kotlin/SimVillage.kt
+++ src/main/kotlin/SimVillage.kt
@@ -1,5 +1,7 @@
-fun main() {
+import kotlin.random.Random
+import kotlin.random.nextInt
 
+fun main() {
     println(
         {
             val currentYear = 2018
@@ -7,10 +9,10 @@ fun main() {
         }()
     )
 
-        val d: Int
+    val d: Int
     //==========================
-    val greatingFunction: () -> String = {
-        val currentYear=2018
+    val greatingFunction = {
+        val currentYear = 2018
         "Welcome to SimVallage - $currentYear"
 
     }
@@ -18,9 +20,90 @@ fun main() {
     println(greatingFunction())
     //=============================
 
-    val greatingFunctionWithPlayerName : (String) -> String = { playerName ->
-        val year=2018
+    val greatingFunctionWithPlayerName: (String) -> String = { playerName ->
+        val year = 2018
         "welcom $playerName - $year"
     }
     println(greatingFunctionWithPlayerName("zamel"))
+
+
+    ///////
+    // input -> integers
+    // output -> bool
+    val isUrgent: (Int) -> Boolean = { age ->
+        age >= 65
+    }
+    var result = isUrgent(90)
+
+    val isUrgentS = { age: Int, name: String ->
+        if (age >= 65)
+            "$name is urgent"
+        else
+            "$name is Not urgent"
+    }
+
+    println(isUrgentS(90, "zamel"))
+
+    //================================
+
+
+    var checkSLetterFun: (Char) -> Boolean = { char ->
+        char == 'h' && char == 'd'
+    }
+    println(checkSLetterFun('4'))
+
+    var count = "zame  dhwebhghdbw h dh dh dh k".count {
+        it == 's'
+    }
+    println(count)
+
+
+    val greetingFunction = { playerName: String, numBuildings: Int ->
+        val currentYear = 2018
+        println("Adding $numBuildings houses")
+        "Welcome to SimVillage, $playerName! (copyright $currentYear)"
+    }
+
+    val hhhhfun = { playerName: String, numBuildings: Int ->
+        val currentYear = 2018
+        println("Adding $numBuildings houses")
+        "refghbj to SimVillage, $playerName! (copyright $currentYear)"
+    }
+    val oooofunb =
+
+    println(greetingFunction)
+    println(greetingFunction("xc", 20))
+    println(runSimulation("zamel", greetingFunction))
+    println("---------------------------------------------")
+//    println(runSimulation("za     mel", ::x))
+    println("---------------------------------------------")
+
+    runSimulation("zamel"){ playerName: String, numBuildings: Int ->
+        val currentYear = 2018
+        println("Adding $numBuildings houses")
+        "Welcome to dfghvbj, $playerName! (copyright $currentYear)"
+    }
+
+
+    var runSim=runSimulation("iluhiousgiwerfhiueruihjh",greetingFunction)
+    println(runSim("sss"))
+}
+
+inline fun runSimulation(playerName: String, zamelFunction: (String, Int) -> String): (String)->String {
+    var buildingNo = Random.nextInt(1..3)
+   return { name->
+       println(name)
+       " $buildingNo zamel ${buildingNo+1}"
+   }
+
+
+
+
+
+
+}
+
+
+fun  x (zz:String): String{
+    return "gfchvjbknm"
 }
