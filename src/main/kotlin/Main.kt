e46e5f41ffa0105dd78869c89ff523462f9052f5
Zamel <mohamed.zamel@tuwaiq.edu.sa>
2021-10-18 16:26:00 +0300

    -listing expamles

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,50 @@
-fun main(){
-    welcomeStudent("Zamel")
-//val result=isPassed(40)
-//    println(result)
+import java.io.File
 
+fun main() {
+// create list of students  min 4 students
+    var listFromFile = File("data/students.txt").readText().split("\n")
+    var listOfStudent= listFromFile.toMutableList()
+    //  ->  var listOfStudent = mutableListOf("zo2la", "bor3y", "ellmby", "z3tot")
+    // Add me , Eng Sadun as  student -> awl el list
+    // Add ur self
+    listOfStudent.add(0, "zamel")
+    listOfStudent.add(1, "Sadun")
+    //  println(listOfStudent)
+    // edit from Zamel l Eng.Mohamed Zamel ,
+    //  listOfStudent[0]="Eng Zamel"
+
+    renameToEng(listOfStudent, "Sadun")
+    renameToEng(listOfStudent, "zamel")
+
+
+    // println(listOfStudent)
+// Sadun . Eng. Hussien Sadun
+
+    // remove ay esm fyh ENG .
+    listOfStudent.removeIf { it.contains("Eng") }
+    //  println(listOfStudent)
+//    listOfStudent.forEachIndexed { index, student ->
+//        listOfStudent[index] = "Stuudent ${listOfStudent[index]}"
+//
+//    }
+
+    for (index in 0 until listOfStudent.size){
+        listOfStudent[index] = "Stuudent ${listOfStudent[index]}"
+    }
+    println(listOfStudent)
+
+    // 3dy 3la el list kol el student esmo Student ...
+    //exmpele List  X y Z
+    //new List STudent X student y stuent Z
+
+    // read student name from the file
+    println(listOfStudent)
+    val (stu1,stu2,_,stu4)=listOfStudent
+    println("$stu2 $stu4")
 }
 
-//non return single expression
-fun welcomeStudent(studentName:String)= println("Welcome $studentName to our course")
-// return value
-fun getPI():Double{
-    return 3.14
+private fun renameToEng(listOfStudent: MutableList<String>, name: String) {
+    var indexOFEng = listOfStudent.indexOf(name)
+    listOfStudent[indexOFEng] =
+        "Eng ${listOfStudent[indexOFEng]}"
 }
-// return value with single exp
-fun getPISingleWay():Double=3.14
-
-fun isPassed(studentGrade:Int)= studentGrade>=50
-
-
-
