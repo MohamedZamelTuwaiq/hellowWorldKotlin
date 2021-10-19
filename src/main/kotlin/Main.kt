669cefc8fc31a9b36ebc9bedb0a059badadfae36
Zamel <mohamed.zamel@tuwaiq.edu.sa>
2021-10-19 18:49:42 +0300

    --strings

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,18 @@
-fun main(){
-    welcomeStudent("Zamel")
-//val result=isPassed(40)
-//    println(result)
+fun main() {
+var orignalStatment="I go to \"school\" by bus"
+    println(orignalStatment)
+//    var indexOfApostrophe=stat.indexOf(' ')
+//    println(indexOfApostrophe)
+    var statAsList=orignalStatment.split(" ")
+
+    println(statAsList)
+    println("${statAsList.get(1)}  length is ${statAsList.get(1).length}")
+//    var theSubString=stat.substring(0 until indexOfApostrophe)
+//    println(theSubString)
+
+  var newStat=orignalStatment.replace('o','p')
+    println(newStat)
 
 }
-
-//non return single expression
-fun welcomeStudent(studentName:String)= println("Welcome $studentName to our course")
-// return value
-fun getPI():Double{
-    return 3.14
-}
-// return value with single exp
-fun getPISingleWay():Double=3.14
-
-fun isPassed(studentGrade:Int)= studentGrade>=50
-
 
 
